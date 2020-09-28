533ede99bdbdb4f262452b6f63ebe412fc0e951e
Brahim SAHBI <brahim.sahbi@gmail.com>
2020-09-28 19:10:11 +0200

    Submit contact does not crash but does not send email

--- server.py
+++ server.py
@@ -1,26 +1,18 @@
-from flask import Flask, render_template
+from os import urandom
+from flask import Flask, render_template, redirect, url_for, flash
 app = Flask(__name__)
 
-# To be removed in  prod
-# I use this in development to apply changes I do on the browser
-# Otherwise only the cached file is shown
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
+app.secret_key = urandom(16)
 @app.route('/')
 def default():
     return render_template('index.html')
 @app.route('/index.html')
 def index():
     return render_template('index.html')
+@app.route('/email.html')
+def email():
+    flash("Message sent")
+    return redirect(url_for('contact'))
 @app.route('/contact.html')
 def contact():
     return render_template('contact.html')
