b2c3d54893b740ecc125b54e6f40a14838d978a2
Brahim SAHBI <brahim.sahbi@gmail.com>
2020-10-08 15:35:09 +0200

    Add contacting with mail skeleton

--- server.py
+++ server.py
@@ -1,18 +1,31 @@
 from os import urandom
 from flask import Flask, render_template, redirect, url_for, flash
+from flask_mail import Mail, Message
+
+mail = Mail()
 app = Flask(__name__)
+mail.init_app(app)
 
 app.secret_key = urandom(16)
 @app.route('/')
 def default():
     return render_template('index.html')
+
 @app.route('/index.html')
 def index():
     return render_template('index.html')
+
 @app.route('/email.html')
 def email():
-    flash("Message sent")
+    #msg = Message("Hello",
+    #        sender="website",
+    #        recipients=["brahim.pro@protonmail.com"])
+    #msg.body = "testing"
+    #msg.html = "<b>testing</b>"
+    #mail.send(msg)
+    flash("Fonctionalité indisponible", "danger")
     return redirect(url_for('contact'))
+
 @app.route('/contact.html')
 def contact():
     return render_template('contact.html')
